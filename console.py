f9f6d031c9fd46a5e619c75fc1f731338d0d3d2c
ZikaZz <92736935+ZikaZz@users.noreply.github.com>
2022-03-01 10:30:21 +0200

    Moved IF Condition into main_screen

--- console.py
+++ console.py
@@ -6,7 +6,15 @@ def main_screen():
     print("Hello, Welcome to PDF Merging and Separating Application . ")
     print(
         "You can: \n 1- Merge 2 PDF Files \n 2- Extract a Page From PDF File \n 3- Separate all Pages in a PDF File ""\n 4- Exit")
-    main_screen.user_input = int(input("Enter Number Of Operation You Need : "))
+    user_input = int(input("Enter Number Of Operation You Need : "))
+    if user_input == 1:
+        merge_pdf_screen()
+    if user_input == 2:
+        extract_page_screen()
+    if user_input == 3:
+        separate_pages_screen()
+    if user_input == 4:
+        print("\nThanks For Using Our Application.")
 
 
 def merge_pdf_screen():
@@ -36,12 +44,3 @@ def separate_pages_screen():
     pdf_file_path = input("Enter PDF File Path : ")
 
 
-main_screen()
-if main_screen.user_input == 1:
-    merge_pdf_screen()
-if main_screen.user_input == 2:
-    extract_page_screen()
-if main_screen.user_input == 3:
-    separate_pages_screen()
-if main_screen.user_input == 4:
-    print("\nThanks For Using Our Application.")
